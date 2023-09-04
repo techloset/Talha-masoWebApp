d42c1d56c28e8aac40e7ab384f48a22afc0f20ba
talhaunas1 <mtalha4600@gmail.com>
2023-09-04 18:46:20 +0500

    working on about section

--- app/pages/about/page.js
+++ app/pages/about/page.js
@@ -1,9 +1,117 @@
 import React from 'react'
+import Image from 'next/image'
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
+    return (
+        <div className='bg-green-500 absolute -z-10 top-12 px-[20px] '>
+            <p className='text-[25.6px] py-[15px]'>About</p>
+            <p className='text-[22.85px]  '>We have an optimistic view of humanity, and we believe in the potency of using design as a tool for actionable change.</p>
+            <p className='text-[12.8px] text-[#6E6E6E] my-[18px] '>At Mason Studio, we design thoughtfully and care deeply, guided by our innate need to “make better”.  This is not just to affect the world but to understand both the opportunities we have and the possibilities we can create to bring real value to society, an experience or a personal moment in a meaningful way. </p >
+            <p className='text-[12.8px] text-[#6E6E6E] my-[18px]'>Our team is diverse academically, culturally and personally - and we bring our whole selves to each of our projects to express the richness of humanity and enhance the value of the design experience.  While all our designers are trained in interior design, we have expertise in architecture, furniture design, human sciences, art consultancy,  art history, fine arts, textile design, graphic design, business, finance and economics.  </p >
+            <p className='text-[12.8px] text-[#6E6E6E] my-[18px]'>We have had the pleasure of partnering with worldwide clients to co-create unconventional and successful solutions.  </p>
+
+            <p className='text-[12.8px] text-[#6E6E6E] my-[18px]'>Select clients include:</p>
+            <p className='text-[12.8px] text-[#6E6E6E] my-[18px]'>
+                Oxford Properties<br />Gairloch<br />RioCan<br />Hazelview Properties<br />Great Gulf<br />Lifetime Developments<br />TAS<br />Daniels Corporation<br />Slate Asset Management<br />Marlin Spring  <br />
+                China Resources<br />Equinox<br />Alo Food Group<br />Hyatt Hotels<br />Kimpton Hotels<br />Viceroy Hospitality Group<br />Kemmons Wilson<br />Innvest Hotels<br />Valor Hospitality Partners
+            </p>
+            {/* image section */}
+
+
+            <Image src='/images/aboutt2.jpg' width='100' height='100' className=' w-full ' />
+            <div className='my-[20px] border-b-[1px] border-indigo-500 '> </div>
+            <Image src='/images/about1.jpg' width='100' height='100' className=' w-full mt-[10px]' />
+            <div className='my-[20px] border-b-[1px] border-indigo-500 '> </div>
+
+
+            {/* text */}
+            <p> Co-Founder, Executive Director</p>
+            <p>Ashley Rumsey</p>
+            <p>BID Hons, NCIDQ, IDC, ARIDO, LEED AP</p>
+            <p>Ashley Rumsey brings the highest standards to both the design vision of projects and the management of the Mason Studio team. An advocate of purposeful design, Ashley has been a key part of the development of Mason Studio’s innovative research-based process. She ensures design decisions are thoughtfully evaluated based on their potential contribution to the end user, design process, and client’s goals. Her collaborative approach ensures a broad-based perspective, leveraging the skills and knowledge of the entire team to fully inform each design project.
+                As a respected leader in the Canadian design community, Ashley has also been featured in international design media and regularly participates in thought leadership at notable conferences and events.</p>
+
+
+            <p> Co-Founder, Creative Director</p>
+            <p>Stanley Sun</p>
+            <p>BID Hons, BSC Hons, NCIDQ, IDC, ARIDO, BCIN</p>
+            <p>Stanley Sun’s vision is informed by a unique blend of formal studies in human sciences, fine arts and interior design.  Approaching each challenge with a combined scientific and humanistic perspective, he first observes how people experience and react to the built environment to create a rational and intuitive design solution. Of particular interest to Stanley’s practice is the science of light and the physiological and psychological response people have to light.As an advocate for Canadian design, Stanley has been published widely and has participated as a keynote speaker at leading design conferences. He is also a sessional instructor at his alma mater, Toronto Metropolitan University.</p>
+
+            <div className='my-[20px] border-b-[1px] border-indigo-500 '> </div>
+
+            <p>Awards</p>
+            <ul>
+                <li>
+                    2020-ACD Awards / Resilient Reatial Concept
+                </li>
+                <li>
+                    2011-ARIDO / Award of Merit: Cabin Residences
+                </li>
+                <li>
+                    2018-ACD Awards / Resilient Reatial Concept
+                </li>
+                <li>
+                    2016-ACD Awards / Resilient Reatial Concept
+                </li>
+                <li>
+                    2015-ARIDO / Award of Merit - Virtuous Pie
+                </li>
+                <li>
+                    2014-Canadian Interiors / Best of Canada - PurParket
+                </li>
+                <li>
+                    2013-ACD Awards / Resilient Reatial Concept
+                </li>
+                <li>
+                    2012-Best of Canada Awards / Andaz Hotel
+                </li>
+                <li>
+                    2011-ARIDO / Award of Merit: Cabin Residences
+                </li>
+                <li>
+                    2010-ACD Awards / Resilient Reatial Concept
+                </li>
+            </ul>
+            <div className='my-[20px] border-b-[1px] border-indigo-500 '> </div>
+
+            <ul>
+                <li>2023 - Office Snapshots / Mason Studio Integrated Workspace</li>
+                <li>2023 - Trend Hunter / Mason Studio Redesigned Its Toronto Office For "The Greater Good"</li>
+                <li>2023 - BluPrint Magazine / Work and Inspiration: Mason Studio’s Office as an ideal Workspace</li>
+                <li>2023 - FRAME Magazine / Can workplaces foster social wellbeing beyond their own employees?</li>
+                <li>2023 - Canadian Interiors / Creating Engagement - May/June 2023 cover feature</li>
+                <li>2023 - The Toronto Star / Making Contact across Toronto with award-winning photography</li>
+                <li>2023 - Hotelier Magazine / Hotel bathrooms become a focal point for designers</li>
+                <li>2023 - Dezeen / Mason Studio reimagines its Toronto workspace "for the greater good"</li>
+                <li>2023 - NUVO Magazine / Design Space: Lady Bird Bar by Mason Studio</li>
+                <li>2022 - Interior Design / 10 Questions with Ashley Rumsey and Stanley Sun</li>
+                <li>2022 - Azure / Communal Condo Kitchen</li>
+                <li>2022 - Designlines / Mason Studio thoughtfully designs Regent Park Condominium</li>
+                <li>2022 - Interior Design / Kimpton Banneker Hotel in Washington, D.C.</li>
+                <li>2022 - Wallpaper / Mason Studio put the focus on local artists in the new Kimpton Banneker hotel in Washington, DC</li>
+                <li>2022 - Azure / 8 Inspiring Design Events To Check Out At DesignTO 2022</li>
+                <li>2021 - Forbes / LadyBird Washington DC's most stylish rooftop bar</li>
+                <li>2021 - Interior Design China / Mason Studio's New Work - Runxi Residences</li>
+                <li>2021 - Interior Design / Big Ideas - Mobile Retail</li>
+                <li>2021 - House & Home / Tommy Smythe - Latest Designers To Turn His Head</li>
+                <li>2021 - Design Milk / Friday Five</li>
+                <li>2020 - Azure / Rethinking Retail</li>
+                <li>2020 - Designlines / Show Us Where You Live</li>
+                <li>2020 - Interior Design / Mason Studio Designs Minimalist Shanghai Interior</li>
+                <li>2020 - Pure Luxury / The Beauty of Canadian Design</li>
+                <li>2019 - Design Anthology / New Hotels - Kimpton Saint George</li>
+                <li>2019 - Design Milk / Kimpton Saint George Hotel</li>
+                <li>2019 - Toronto Life / Kimpton Saint George Hotel</li>
+                <li>2018 - Wallpaper / Kimpton Saint George Hotel</li>
+                <li>2018 - New York Times / An Ottawa Hotel is an ode to Canada</li>
+                <li>2018 - Canadian Interiors / Best of Canada: Hospitality, Andaz Hotel</li>
+                <li>2018 - Canadian Interiors / Best of Canada: Retail, Picnic Food</li>
+
+            </ul>
+
+            <div className='my-[20px] border-b-[1px] border-indigo-500 '> </div>
+        </div>
+    )
 }
 
 export default page
