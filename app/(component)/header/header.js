2830251d42197e5b0ff7d0fdb5f476464a70968d
talhaunas1 <mtalha4600@gmail.com>
2023-09-06 01:32:17 +0500

    done with thw mobile responsiveness

--- app/(component)/header/header.js
+++ app/(component)/header/header.js
@@ -7,7 +7,7 @@ import Link from 'next/link'
 
 const header = () => {
 
-    const [navbar, setNavbar] = useState(false)
+    const [navbar, setNavbar] = useState(true)
     return (
         <div className='   '>
 
@@ -17,55 +17,68 @@ const header = () => {
                     navbar ? (
                         <div className='  bg-[#F2CCBA] w-full h-screen  '>
 
-                            <div className=' flex justify-around items-center px-[8px] '>
+                            <div className=' flex justify-around   items-center px-[20px] '>
                                 {/* mason logo */}
-                                <div>
-                                    <Image src='/images/mason.svg' height='100' width='100' />
+                                <div className=''>
+                                    <Link href='/' >  <Image src='/images/mason.svg' alt="mason logo" height='100' width='100' className=' cursor-pointer w-[70%] ' /> </Link>
                                 </div>
 
-
-                                <div className=' w-[50%] flex justify-end mt-[10px] p-[10px] ' >
+                                <div className=' w-[100%] flex justify-end mt-[10px]  p-[15px]' >
                                     <Image onClick={() => setNavbar(!navbar)} src='/images/cross.svg' width='100' height='100' className='  w-[20px] h-[20px] cursor-pointer  ' />
+
                                 </div>
+
+
+                                {/* <div className=' w-[50%] flex justify-end mt-[10px] p-[10px] ' >
+                                    <Image onClick={() => setNavbar(!navbar)} src='/images/cross.svg' width='100' height='100' className='  w-[20px] h-[20px] cursor-pointer  ' />
+                                </div> */}
 
                             </div>
 
                             <ul className='   px-[15px] '>
 
-                                <div className='border-t-[1px] border-gray '></div>
-                                <Link href='/pages/projects' onClick={() => setNavbar(!navbar)}> <li className='text-[25.6px] mt-[9px] mb-[7px] text-[#00000080] '>Projects</li> </Link>
+                                {/* horiozntal line */}
+                                <div className='border-t-[1px] border-white '></div>
 
-                                <div className='border-t-[1px] border-gray '></div>
+                                <div className='sm:flex   '>
+                                    <Link href='/pages/projects' onClick={() => setNavbar(!navbar)} className=' sm:w-[50%]'> <li className='text-[25.6px] mt-[9px] mb-[7px] text-[#00000080] '>Projects</li> </Link>
 
-                                <Link href='/pages/about' onClick={() => setNavbar(!navbar)}> <li className='text-[25.6px] mt-[9px] mb-[7px] text-[#00000080]' > About</li> </Link>
+                                    <div className='border-t-[1px] border-white sm:hidden'>  </div>
 
-                                <div className='border-t-[1px] border-gray '></div>
+                                    <Link href='/pages/about' onClick={() => setNavbar(!navbar)} className=' sm:w-[50%]'> <li className='  text-[25.6px] mt-[9px] mb-[7px] text-[#00000080]' > About</li> </Link>
+                                </div>
 
-                                <Link href='/pages/culturalhub' onClick={() => setNavbar(!navbar)}>  <li className='text-[25.6px] mt-[9px] mb-[7px] text-[#00000080]' >Culture Hub</li> </Link>
+                                {/* horizontal line */}
+                                <div className='border-t-[1px] border-white '></div>
 
-                                <div className='border-t-[1px] border-gray '></div>
+                                <div className='sm:flex '>
+                                    <Link href='/pages/culturalhub' onClick={() => setNavbar(!navbar)} className=' sm:w-[50%]'>  <li className='text-[25.6px] mt-[9px] mb-[7px] text-[#00000080]' >Culture Hub</li> </Link>
 
-                                <Link href='/pages/journal' onClick={() => setNavbar(!navbar)}> <li className='text-[25.6px]  mt-[9px] mb-[7px] text-[#00000080]  ' >Journal</li> </Link>
+                                    <div className='border-t-[1px] border-white sm:hidden'></div>
 
-                                <div className='border-t-[1px] border-gray '></div>
+                                    <Link href='/pages/journal' onClick={() => setNavbar(!navbar)} className=' sm:w-[50%]'> <li className='text-[25.6px]  mt-[9px] mb-[7px] text-[#00000080]  ' >Journal</li> </Link>
+                                </div>
+
+                                {/* horizontal line */}
+                                <div className='border-t-[1px] border-white '></div>
 
 
 
                                 {/* footer */}
 
-                                <div className=' text-[12.8px] pt-[25px]    '>
+                                <div className=' text-[12.8px] pt-[25px]   sm:mt-[120px]'>
 
-                                    <div className='border-t-[1px] border-gray  mt-[50px]  '></div>
+                                    <div className='border-t-[1px] border-white  mt-[50px] sm:w-[45%] S'></div>
 
-                                    <div className='flex justify-start  mt-[20px] '>
+                                    <div className='flex justify-start   mt-[20px] '>
 
-                                        <div className=' w-[50%] text-[#000000]'>
+                                        <div className='sm:absolute sm:right-[15px] sm:bottom-[135px] sm:w-[45%] S w-[50%] text-[#000000]'>
                                             <p className=''>+1 (416) 761-9736 </p>
                                             <p >Follow Us </p>
                                             <p>Send us an Email </p>
                                         </div>
 
-                                        <div className='  text-[#000000]'>
+                                        <div className='sm:w-[45%] sm:absolute sm:left-3 sm:bottom-[85px]S  text-[#000000]'>
                                             <p className=''>91 Pelham Avenue</p>
                                             <p >Toronto, Canada</p>
                                             <p>M6N 1A5</p>
@@ -73,9 +86,9 @@ const header = () => {
 
                                     </div>
 
-                                    <div className='border-t-[1px] border-gray mt-[10px]'></div>
+                                    <div className='border-t-[1px] border-white mt-[10px] sm:w-[45%] sm:absolute sm:right-[17px]'></div>
 
-                                    <div className='flex gap-3 mt-[10px]'>
+                                    <div className='flex gap-3 mt-[10px] sm:absolute sm:right-[15px] sm:bottom-[100px] sm:w-[45%] S'>
                                         <p>工作室</p>
                                         <p>/</p>
                                         <p>微信</p>
